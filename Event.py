e2653ed79ba1c32e307eb84cc6655badd0b5ab49
Alexander Sh <sanchaez@hotmail.com>
2017-06-13 16:14:57 +0300

    * Implemented Event class
    Most of class was implemented.

--- Event.py
+++ Event.py
@@ -1,2 +1,21 @@
 class Event:
-    pass
+    def __init__(self, title, description):
+        self.title = title
+        self.description = description
+
+    @property
+    def title(self):
+        return self.__title
+
+    @property
+    def description(self):
+        return self.__description
+
+    @description.setter
+    def description(self, description):
+        self.__description = description
+
+    @title.setter
+    def title(self, title):
+        self.__title = title
+
