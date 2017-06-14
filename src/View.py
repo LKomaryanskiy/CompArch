696b4c06d01e4ef8aed5b42738c7135c0a28ed10
DmytroBevz <dima_145@ukr.net>
2017-06-14 20:11:54 +0300

    Added static property for view methods

--- src/View.py
+++ src/View.py
@@ -1,5 +1,6 @@
 class View:
 
+    @staticmethod
     def main_menu():
         print("~~ Menu ~~")
         print("1: Add event")
@@ -7,9 +8,11 @@ class View:
         print("3: List of events")
         print("4: Exit\n")
 
+    @staticmethod
     def event_create():
         print("Enter the information about the event below\n.")
 
+    @staticmethod
     def print_events(events_list):
         if not events_list:
             print('You have no events!')
@@ -20,14 +23,18 @@ class View:
                 print("{}. {}".format(i + 1, event))
         print('\n')
 
+    @staticmethod
     def success_event_create_message():
         print("\nEvent set!\n")
 
+    @staticmethod
     def wrong_input():
         print("Invalid input!\n")
 
+    @staticmethod
     def wrong_option():
         print("No current option!\n")
 
+    @staticmethod
     def exit_message():
         print("We saved your data. Be organized!\n")
