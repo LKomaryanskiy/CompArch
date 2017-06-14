61f0facc467f99d1ce3f5d7beda27aa819990440
Leonid Komaryanskiy <lkomaryanskiy@gmail.com>
2017-06-14 10:01:35 +0300

    Replace all source files to src

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
-from src.Controller import Controller
-from src.Model import Model
+from Controller import Controller
+from Model import Model
 
 if __name__ == "__main__":
     model = Model('savedata', result_dict[save_type_key])
